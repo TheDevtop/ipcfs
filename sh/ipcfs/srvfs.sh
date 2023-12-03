e7ee720a5407e2793adc2fc4a90a4f5d08cb6ef2
Thijs Haker <theage2001@gmail.com>
2023-12-03 12:59:34 +0100

    Upgraded go packages, added comments

--- sh/ipcfs/srvfs.sh
+++ sh/ipcfs/srvfs.sh
@@ -3,16 +3,19 @@
 # Library for use with srvfs
 # Tested with ksh on NetBSD
 
+# srvfs.register(name)
 srvfs.register() {
         mkfifo -m 666 /srv/$1
         return
 }
 
+# srvfs.deregister(name)
 srvfs.deregister() {
         rm /srv/$1
         return
 }
 
+# srvfs.read(name) -> string
 srvfs.read() {
         cat /srv/$1
         return
